d1ab45ac7b28901d235bd6504f5685ff4c3e03ca
BelkinaDasha <belkinadasha@gmail.com>
2019-09-24 16:20:55 +0300

    test: quick start

---
+++ test/demo.js
@@ -0,0 +1,25 @@
+const assert = require('assert');
+const rp = require('request-promise');
+const DemoApp = require('../index');
+
+describe('server', () => {
+  it('should be able to start', async () => {
+    const demoApp = new DemoApp();
+    await demoApp.connect();
+    await demoApp.close();
+  });
+
+  it('should say hello world', async () => {
+    const demoApp = new DemoApp();
+    await demoApp.connect();
+
+    try {
+      const response = await rp({
+        uri: 'http://0.0.0.0:3000/demo',
+      });
+      assert(response, 'Hello, world');
+    } finally {
+      await demoApp.close();
+    }
+  });
+});
