2b36fd238a6208db2e2e9065421239b213778ccd
pajgo <pavel.rogovoi@gmail.com>
2019-11-08 17:02:45 +0600

    feat: validator
    * semicolon remove
    * restructure

---
+++ src/actions/validation/query-string.js
@@ -0,0 +1,34 @@
+const { ActionTransport } = require('@microfleet/core')
+
+function listTodo(request) {
+  return { query : request.query}
+}
+
+listTodo.transports = [ActionTransport.http]
+listTodo.transportsOptions = {
+  [ActionTransport.http]: {
+    methods: ['get'],
+  },
+}
+listTodo.transformQuery = (query) => {
+  query.page *= 1
+
+  switch (query.hidden) {
+    case 'true':
+    case '1':
+      query.hidden = true
+      break
+
+    case 'false':
+    case '0':
+      query.hidden = false
+      break
+    default:
+      query.hidden = null
+  }
+  return query
+}
+
+listTodo.schema = 'todo.list'
+
+module.exports = listTodo
--- test/validation.js
+++ test/validation.js
@@ -1,23 +1,23 @@
-const assert = require('assert');
-const rp = require('request-promise');
-const DemoApp = require('../src');
+const assert = require('assert')
+const rp = require('request-promise')
+const DemoApp = require('../src')
 
 describe('#todo validation', () => {
   before(async () => {
-    this.demoapp = new DemoApp();
-    await this.demoapp.connect();
-  });
+    this.demoapp = new DemoApp()
+    await this.demoapp.connect()
+  })
 
   after(async () => {
-    await this.demoapp.close();
-  });
+    await this.demoapp.close()
+  })
 
   describe('query string validation', () => {
     const listTodo = rp.defaults({
-      uri: 'http://0.0.0.0:3000/todo/list',
+      uri: 'http://0.0.0.0:3000/validation/query-string',
       method: 'GET',
       json: true,
-    });
+    })
 
     it('throws validation error on incorrect data', async () => {
       const { error } = await listTodo({
@@ -25,30 +25,30 @@ describe('#todo validation', () => {
           page: 0,
         },
       }).catch((e) => {
-        return e;
-      });
+        return e
+      })
 
-      assert(error.statusCode === 400, 'must throw validation error');
+      assert(error.statusCode === 400, 'must throw validation error')
       assert(error.message === 'todo.list validation failed: data.page should be >= 1, data.hidden should be boolean')
-    });
+    })
 
     it('performs some work when qs is valid', async () => {
       const qs = {
         page:10,
         hidden: true,
-      };
-      const response = await listTodo({ qs });
+      }
+      const response = await listTodo({ qs })
 
-      assert.deepStrictEqual(response.query, qs, 'returns parsed qs as result');
-    });
-  });
+      assert.deepStrictEqual(response.query, qs, 'returns parsed qs as result')
+    })
+  })
 
   describe('request body validation', () => {
     const createTodo = rp.defaults({
-      uri: 'http://0.0.0.0:3000/todo/create',
+      uri: 'http://0.0.0.0:3000/validation/schema',
       method: 'POST',
       json: true,
-    });
+    })
 
     it('throws validation error on incorrect data', async () => {
       const { error } = await createTodo({
@@ -56,34 +56,34 @@ describe('#todo validation', () => {
           todo: {},
         },
       }).catch((e) => {
-        return e;
-      });
+        return e
+      })
 
-      assert(error.statusCode === 400, 'must throw validation error');
-      assert(error.name === 'HttpStatusError');
+      assert(error.statusCode === 400, 'must throw validation error')
+      assert(error.name === 'HttpStatusError')
       assert(error.message === 'todo.create validation failed: data.todo should have required property \'state\', data.todo should have required property \'name\'')
-    });
+    })
 
     it('performs some work when data is valid', async () => {
       const todo = {
         name: "myTodo",
         description: "my description",
         state: true,
-      };
+      }
 
       const { ok } = await createTodo({
         body: { todo },
-      });
-      assert.deepStrictEqual(ok.todo, todo, 'returns todo as success result');
-    });
-  });
+      })
+      assert.deepStrictEqual(ok.todo, todo, 'returns todo as success result')
+    })
+  })
 
   describe('object validation', () => {
     const validateTodo = rp.defaults({
-      uri: 'http://0.0.0.0:3000/todo/validate',
+      uri: 'http://0.0.0.0:3000/validation/validate',
       method: 'POST',
       json: true,
-    });
+    })
 
     it('returns validation error on incorrect data', async () => {
       const { validationError } = await validateTodo({
@@ -93,24 +93,24 @@ describe('#todo validation', () => {
           },
         },
       }).catch((e) => {
-        return e;
-      });
+        return e
+      })
 
-      assert(validationError.name === 'HttpStatusError');
+      assert(validationError.name === 'HttpStatusError')
       assert(validationError.message === 'objects.todo validation failed: data should have required property \'state\'')
-    });
+    })
 
     it('performs some work when data is valid', async () => {
       const todo = {
         name: "myTodo",
         description: "my description",
         state: true,
-      };
+      }
 
       const response = await validateTodo({
         body: { todo },
-      });
-      assert.deepStrictEqual(response.validationResult, todo, 'returns todo as success result');
-    });
-  });
-});
+      })
+      assert.deepStrictEqual(response.validationResult, todo, 'returns todo as success result')
+    })
+  })
+})
