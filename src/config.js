d7602dd5a2bec31c0842261f01e36cdd30ac8f41
BelkinaDasha <belkinadasha@gmail.com>
2019-11-12 12:11:39 +0000

    feat: linter

--- src/config.js
+++ src/config.js
@@ -8,6 +8,6 @@ module.exports = {
       $filter: 'env',
       // we expect that production value would be passed in the production env so we leave it undefined
       test: 'i-am-NOT-a-secret',
-    }
-  }
+    },
+  },
 }
--- src/index.js
+++ src/index.js
@@ -12,4 +12,4 @@ class DemoApp extends Microfleet {
   }
 }
 
-module.exports = DemoApp;
+module.exports = DemoApp
